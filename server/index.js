ed9fc392bfd601385d2d655365a0ec7f22ad5559
Ewuru Freeborn <109674539+15sxope@users.noreply.github.com>
2023-08-19 20:54:14 +0100

    add cors origin and credentials

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 
 const express = require('express');
+
+const app = express();
 const cors = require('cors');
 app.use(cors({origin:true,credentials: true}));
-const app = express();
-
 app.use(cors());
 app.use(express.json());
 
