0c8e900799a271f0a7242f6d3e141b277518310d
Ewuru Freeborn <109674539+15sxope@users.noreply.github.com>
2023-08-21 03:03:36 +0100

    change sandbox environment to LiveEnvironment, add real paypal.com link

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const paypal = require('@paypal/checkout-server-sdk');
 const clientId = process.env.PAYPAL_CLIENT_ID;
 const clientSecret = process.env.PAYPAL_CLIENT_SECRET;
 
-const environment = new paypal.core.SandboxEnvironment(clientId, clientSecret);
+const environment = new paypal.core.LiveEnvironment(clientId, clientSecret);
 const client = new paypal.core.PayPalHttpClient(environment);
 
 const storeItems = new Map([
